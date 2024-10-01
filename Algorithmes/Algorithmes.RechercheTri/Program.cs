e196f4db92a64e5444d639e9b3f5609eefa72985
William Nanfack <william.nanfack@hudi.it>
2024-10-01 05:27:54 +0200

    Check thread

---
+++ Algorithmes/Algorithmes.RechercheTri/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algorithmes.RechercheTri;
+
+Console.WriteLine("Hello, World!");
+
+//algoritme de recherche des X dernière ligne d'un fichier
+    //Console.WriteLine("RechercheDansFichier.GetLastXRows");
+//string path = @"C:\Users\WilliamNanfack\Downloads\textlorem.txt";
+
+//Console.WriteLine("voulez vous commencer Oui 'o' ou Non 'n'");
+//var reponse = Console.ReadKey().Key == ConsoleKey.O; // rechecher n fois
+//while (reponse)
+//{
+//    //reponse = RechercheDansFichierOrArray.GetLastXRows(null, path);
+//    //reponse = RechercheDansFichierOrArray.GetElementDistinct(false);
+//}
+
+//RechercheDansFichierOrArray.SayHelloBoy();
+
+
+Task tsk1 = Task.Run(() => RechercheDansFichierOrArray.SayHello("task1 "));
+Task tsk2 = Task.Run(() => RechercheDansFichierOrArray.SayName("task2 "));
+
+Task.WaitAll(tsk1, tsk2);
+
+//Thread thread1 = new Thread(RechercheDansFichierOrArray.SayHello);
+//Thread thread2 = new Thread(RechercheDansFichierOrArray.SayName);
+
+//thread1.Start();
+//thread2.Start();
+
+//thread1.Join();
+//thread2.Join();
+
+Console.WriteLine("\n\t voulez vous continuer Oui 'O' ou Non 'N'");
+//if(Console.ReadKey().Key != null) Environment.Exit(1);
+//OR
+Console.ReadLine();
---
+++ Algorithmes/Algorithmes.RechercheTri/RechercheDansFichierOrArray.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Algorithmes.RechercheTri
+{
+    public class RechercheDansFichierOrArray
+    {
+        public static bool GetLastXRows(int? n, string path)
+        {
+            int num = 0;
+            if (n == null || n == 0)
+            {
+                Console.WriteLine("/\n\t\tinsert un nombre de line superieru à zero");
+                var x = Console.ReadLine();
+                if (int.TryParse(x, out num))
+                {
+                    Console.WriteLine("bien");
+                }
+                else
+                {
+                    Console.WriteLine("vous n'avez pasinserer un nombre entier");
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                num = n.Value;
+            }
+            var rows = File.ReadAllLines(path);
+            if (rows.Length > 0)
+                return Print(rows.Reverse().Take(num).Reverse().ToList());
+            else
+            {
+                Console.WriteLine("le fichier est vide ou n'existe pas");
+                return false;
+            }
+        }
+        public static bool Print(List<string> lastNrow)
+        {
+
+            if (lastNrow.Count > 0)
+            {
+                foreach (var item in lastNrow)
+                {
+                    Console.WriteLine("  -*-  " + item);
+                }
+            }
+            Console.WriteLine("voulez vous continuer Oui 'O' ou Non 'N'");
+            return Console.ReadKey().Key == ConsoleKey.O;
+        }
+
+        public static bool GetElementDistinct(bool yes)
+        {
+            string[] arrA = ["a", "e", "e", "e", "b"];
+            string[] arrB = ["b", "b", "c", "e", "e", "g"];
+
+            Console.WriteLine(@"Soit les listes suivantes: A[" + string.Join(", ", arrA) + "] et \t B ["+ string.Join(", ", arrB) + "]");
+            Console.WriteLine("voulez vous trouver des caractère commun Oui 'O' ou Non 'N'");
+            
+            yes = Console.ReadKey().Key == ConsoleKey.O;
+            var distinctArrA = arrA.Distinct().ToList();
+            var distinctArrB = arrB.Distinct().ToList();
+            string text = "";
+            if (yes)
+            {
+                //utilisation des HashSet pour la performance; dans les cas de tableaux volumineux et dont il y'a necessité d'y accéder continuellement
+                HashSet<string> plusLongArray = new(distinctArrB);
+                var result = distinctArrA.Intersect(plusLongArray);
+                text = string.Join(", ", result);
+            }
+            else
+            {
+                var result = distinctArrB.Union(distinctArrA);
+                text = string.Join(", ", result);
+            }
+
+            Console.WriteLine(@"[" + text +"]");
+            Console.WriteLine("voulez vous continuer Oui 'O' ou Non 'N'");
+            return Console.ReadKey().Key == ConsoleKey.O;
+        }
+
+        public static int count = 0;
+        public static readonly object locker = new();
+
+        public static void SayHello(string onThread)
+        {
+            //Console.WriteLine("SayHello {0}", onThread);
+            while (count < 10)
+            {
+                lock (locker)
+                {
+                    Console.Write("Salut ");
+                    count++;
+                    Thread.Sleep(1000);
+                }
+                //await Task.Delay(1000);
+            }
+        }
+        public static void SayName(string onThread)
+        {
+            //Console.WriteLine("SayName {0}", onThread);
+            while (count < 10)
+            {
+                lock (locker)
+                {
+                    Console.Write("William ");
+                    count++;
+                    Thread.Sleep(1000);
+                }
+                //await Task.Delay(1000);
+            }
+        }
+    }
+}
